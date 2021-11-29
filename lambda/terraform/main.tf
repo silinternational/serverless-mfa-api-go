9a86cd87780911d0f7175082eb3099b7e1e8b10d
Phillip Shipley <phillip.shipley@gmail.com>
2021-11-29 16:53:22 -0500

    update lambda role to fix serverless user connection

--- lambda/terraform/main.tf
+++ lambda/terraform/main.tf
@@ -11,7 +11,7 @@ module "serverless-user" {
 
 // Create role for lambda function
 resource "aws_iam_role" "lambdaRole" {
-  name = "${var.app_name}-${var.app_env}-lambdaRole"
+  name = "${var.app_name}-${var.app_env}-${var.aws_region}-lambdaRole"
 
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",

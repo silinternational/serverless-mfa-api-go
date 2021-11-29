4ca9f46832cda02643fa4a6165a0d380ea41496d
Phillip Shipley <phillip.shipley@gmail.com>
2021-11-29 16:17:18 -0500

    fix deploy script and aws config

--- lambda/terraform/outputs.tf
+++ lambda/terraform/outputs.tf
@@ -2,3 +2,7 @@ output "serverless_user_key_secret" {
   value     = "${module.serverless-user.aws_access_key_id},${module.serverless-user.aws_secret_access_key}"
   sensitive = true
 }
+
+output "lambda_role_arn" {
+  value = aws_iam_role.lambdaRole.arn
+}

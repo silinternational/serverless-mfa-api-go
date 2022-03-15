c8a69a3bece073e9cd20636c98e937f949c9e9cd
Schparky <3172830+Schparky@users.noreply.github.com>
2022-03-15 09:31:01 -0600

    don't echo secret to build log

--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -17,7 +17,9 @@ if [ "${CI_BRANCH}" == "develop" ];
 then
   STAGE="dev"
   export AWS_ACCESS_KEY_ID="${STG_AWS_ACCESS_KEY_ID}"
+  set +x
   export AWS_SECRET_ACCESS_KEY="${STG_AWS_SECRET_ACCESS_KEY}"
+  set -x
   export LAMBDA_ROLE="${STG_LAMBDA_ROLE}"
   export API_KEY_TABLE="${STG_API_KEY_TABLE}"
   export WEBAUTHN_TABLE="${STG_WEBAUTHN_TABLE}"
@@ -25,12 +27,14 @@ elif [ "${CI_BRANCH}" == "main" ];
 then
   STAGE="production"
   export AWS_ACCESS_KEY_ID="${PRD_AWS_ACCESS_KEY_ID}"
+  set +x
   export AWS_SECRET_ACCESS_KEY="${PRD_AWS_SECRET_ACCESS_KEY}"
+  set -x
   export LAMBDA_ROLE="${PRD_LAMBDA_ROLE}"
   export API_KEY_TABLE="${PRD_API_KEY_TABLE}"
   export WEBAUTHN_TABLE="${PRD_WEBAUTHN_TABLE}"
 else
-    echo "deployments only happen from develop and main branches"
+    echo "deployments only happen from develop and main branches (branch: ${CI_BRANCH})"
     exit 1
 fi
 

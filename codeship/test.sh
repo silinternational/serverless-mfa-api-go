acfac1cd8b7af085f73523f530f5dc6e4c99e4dc
Schparky <3172830+Schparky@users.noreply.github.com>
2022-03-17 09:28:46 -0600

    fix Codeship build (workdir was throwing things off)

--- codeship/test.sh
+++ codeship/test.sh
@@ -6,6 +6,22 @@ set -e
 # Echo commands to console
 set -x
 
+# export appropriate AWS credentials for `serverless info`
+if [ "${CI_BRANCH}" == "main" ];
+then
+  STAGE="production"
+  export AWS_ACCESS_KEY_ID="${PRD_AWS_ACCESS_KEY_ID}"
+  set +x
+  export AWS_SECRET_ACCESS_KEY="${PRD_AWS_SECRET_ACCESS_KEY}"
+  set -x
+else
+  STAGE="dev"
+  export AWS_ACCESS_KEY_ID="${STG_AWS_ACCESS_KEY_ID}"
+  set +x
+  export AWS_SECRET_ACCESS_KEY="${STG_AWS_SECRET_ACCESS_KEY}"
+  set -x
+fi
+
 # disabled tests due to issue on Codeship resolving dynamo container
 #go test -v ./...
 
@@ -13,4 +29,4 @@ set -x
 serverless --version
 
 # Validate Serverless config
-serverless info
+serverless info --stage "${STAGE}"

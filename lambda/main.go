38c76dccf6fc013e83a7b28da2136dcbfac040e1
Steve Bagwell <steve_bagwell@sil.org>
2022-07-14 14:47:00 -0400

    add delete webauthn/credential to lambda also

--- lambda/main.go
+++ lambda/main.go
@@ -69,6 +69,10 @@ func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.API
 		mfa.FinishRegistration(w, r)
 	case "delete /webauthn/user":
 		mfa.DeleteUser(w, r)
+	// This expects a query string param like `?credential-id=<hashed-encoded-credential-id>
+	// where the id was previously returned as the key_handle_hash from the FinishRegistration call
+	case "delete /webauthn/credential":
+		mfa.DeleteCredential(w, r)
 	default:
 		return clientError(http.StatusNotFound, fmt.Sprintf("The requested route is not supported: %s", route))
 	}

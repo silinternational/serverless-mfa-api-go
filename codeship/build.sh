c07e5c32d88148b76fb4419a8c2ab6291d49c45b
briskt <3172830+briskt@users.noreply.github.com>
2023-11-20 14:04:16 -0700

    add a comment to identify the reason that the binary is named bootstrap

--- codeship/build.sh
+++ codeship/build.sh
@@ -6,5 +6,5 @@ set -e
 # Echo out all commands for monitoring progress
 set -x
 
-# Build the binaries
+# When using the provided.al2 runtime, the binary must be named "bootstrap" and be in the root directory
 CGO_ENABLED=0 go build -tags lambda.norpc -ldflags="-s -w" -o bootstrap ./lambda

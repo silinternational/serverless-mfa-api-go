1feea2334769162ae6c0f6c98f7937f206e6ae90
briskt <3172830+briskt@users.noreply.github.com>
2024-10-16 16:11:33 +0800

    update aws and cloudflare providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ locals {
 */
 module "serverless-user" {
   source  = "silinternational/serverless-user/aws"
-  version = "0.4.2"
+  version = "~> 0.4.2"
 
   app_name           = "${var.app_name}-${var.app_env}"
   aws_region_policy  = "*"
@@ -20,7 +20,7 @@ module "serverless-user" {
 // Set up custom domain name for easier fail-over.
 module "dns_for_failover" {
   source  = "silinternational/serverless-api-dns-for-failover/aws"
-  version = "~>0.5.1"
+  version = "~> 0.6.0"
 
   api_name             = local.api_name
   cloudflare_zone_name = var.cloudflare_domain

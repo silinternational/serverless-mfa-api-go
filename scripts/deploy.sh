56fbac429855dccb26428926abaa93d17988d7f9
briskt <3172830+briskt@users.noreply.github.com>
2025-04-24 09:51:42 +0800

    fix the lack of STAGE variable definition

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,8 +6,7 @@ set -e
 # Echo out all commands for monitoring progress
 set -x
 
-# temporary: see permissions on .git folder to troubleshoot error message "error obtaining VCS status: exit status 128"
-ls -adl .git
+# circumvent Go error message "error obtaining VCS status: exit status 128"
 git config --global --add safe.directory $(pwd)
 
 # Build binaries
@@ -16,5 +15,5 @@ CGO_ENABLED=0 go build -tags lambda.norpc -ldflags="-s -w" -o bootstrap ./lambda
 # Print the Serverless version in the logs
 serverless --version
 
-echo "Deploying stage $STAGE of serverless package to region $1..."
-serverless deploy --verbose --stage "$STAGE" --region "$1"
+echo "Deploying stage $2 of serverless package to region $1..."
+serverless deploy --verbose --stage "$2" --region "$1"
